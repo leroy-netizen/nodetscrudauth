78f1c6b457f59328ecceaf2543337534f4e1d309
leroy-netizen <leroythomas051@gmail.com>
2023-01-11 16:36:32 +0300

    create a database connection file and connect to db

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express from "express";
-import config from 'config';
+import config from "config";
+import connectDb from "./utils/db.connect";
 
-const port = config.get<number>('port');
+const port = config.get<number>("port");
 
-const app = express()
+const app = express();
 
-app.listen(port, () => console.log(`Server running on port ${port}`))
+app.listen(port, async () => {
+  console.log(`Server running on port ${port}`);
+  await connectDb();
+});
