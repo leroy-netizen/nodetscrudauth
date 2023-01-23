e744c27e03cac1dd75db8a85fddb6bd207fdf419
leroy-netizen <leroythomas051@gmail.com>
2023-01-23 11:53:36 +0300

    utility function to compare password with hashed password

---
+++ .history/src/models/user.model_20230111201210.ts
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import config from "config";
+
+export interface UserDocument extends mongoose.Document {
+  email: string;
+  name: string;
+  password: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+export interface HookNextFunction {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  (error?: Error): any;
+}
+
+const userSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+userSchema.pre("save", async function (next: HookNextFunction) {
+  let user = this as UserDocument;
+  if (!user.isModified("password")) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(config.get<number>("saltFactorRound"));
+  const hash = await bcrypt.hashSync(user.password, salt);
+});
+
+const UserModel = mongoose.model("User", userSchema);
+
+export default UserModel;
